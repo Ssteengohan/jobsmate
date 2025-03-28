8d73f547b10c87542008ea3321631457153b318a
Gio Todorevic <gioowie14@gmail.com>
2025-03-28 22:12:53 +0100

    add Tabs component and implement vertical/horizontal lines in Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Navbar from '@/themes/components/Navbar/Navbar';
 import { SpotlightPreview } from '@/themes/components/HeroBanner/HeroBanner';
+import Tabs from '@/themes/components/Tabs/Tabs';
 
 export default function Home() {
   return (
-    <main className="mx-auto">
+    <main className="relative mx-auto min-h-screen overflow-hidden bg-white">
+      <div className="absolute inset-y-0 left-[10%] w-[0.5px] bg-neutral-400 opacity-20"></div>
+      <div className="absolute inset-y-0 left-[90%] w-[0.5px] bg-neutral-400 opacity-20"></div>
+      <div className="absolute inset-x-0 top-[80%] h-px bg-neutral-400 opacity-10"></div>
       <Navbar />
       <SpotlightPreview />
+      <Tabs />
     </main>
   );
 }
---
+++ src/themes/components/Tabs/Tabs.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tabs = () => {
+  return <section className="container mx-auto">Tabs</section>;
+};
+
+export default Tabs;
